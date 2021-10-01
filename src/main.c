7028d7c2e6fab6e5c1e4ef4d24176ff55f0722fc
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-10-01 15:24:30 +0100

    gitignore .vscode, add some headers to main

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
+#include <getopt.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        // TODO
-        printf("Usage: %s \n", argv[0]);
-        return 1;
-    }
+int main(int argc, const char *argv[]) {
+  if (argc < 2) {
+    // TODO
+    printf("Usage: %s \n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-    return 0;
+  return EXIT_SUCCESS;
 }
-
